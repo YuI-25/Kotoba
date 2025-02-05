e01b0b82c3951d8dea93c70264313bc9d173fcf0
Yu <yu.iwaki94@gmail.com>
2025-02-05 15:13:43 +0100

    ajout de afficherscore, correction affichage jeu

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,18 @@
-// 1) quand l'utilisateur choisi le mode de jeux, la lise s'affiche dans la case 
+// 1) quand l'utilisateur choisi le mode de jeux, la lise s'affiche dans la case --ok 
 
 // 2) quand l'utilisateur tape la réponse et cliquer sur vérifier 
-        // comparer avec la reponse 
+        // comparer avec la reponse -- ok 
         // puis changer le score 
 
 // 3) quand l'utilisateur clique sur réviser, on affiche les mots et rep mal répondu 
 
 
-// définition générales 
-let i = 0   // index mot dans la liste 
-let i_correction = 0   // index mot dans la liste correction 
-let score = 0 
-let listeProposition = listeVerbe
-let listeCorrect = listeVerbeRep
-let nbMotsTotal = listeProposition.length
+ // définition générales 
+ let i = 0   // index mot dans la liste 
+ let score = 0 
+ let listeProposition = listeVerbe
+ let listeCorrect = listeVerbeRep
+ let nbMotsTotal = listeProposition.length
 
 
 // 1) on affiche le la liste des mots dans la zone proposition 
@@ -25,11 +24,11 @@ function affichageMots (proposition) {
 affichageMots(listeProposition[i])
 
 
-// Vérif si affichageMots fonctionne avec btn vérifier
-let BtnVerifierMots = document.getElementById("BtnVerifier")
-    BtnVerifierMots.addEventListener("click", ()=>{
-        affichageMots(listeProposition[i])
-    })
+// // Vérif si affichageMots fonctionne avec btn vérifier
+// let BtnVerifierMots = document.getElementById("BtnVerifier")
+//     BtnVerifierMots.addEventListener("click", ()=>{
+//         affichageMots(listeProposition[i])
+//     })
 
 // changer la liste mots en fonction de choix utilisateur et afficher dans la zone proposition 
 function choixTypeJeu(){
@@ -54,55 +53,71 @@ function choixTypeJeu(){
                     listeCorrect = listeAdjectifRep
             }
         
-                affichageMots(listeProposition[i])
+            affichageMots(listeProposition[i])
 
         })
     })
 }
 
-
+// pour afficher les scores 
+function afficherScore(score, nbMotsTotal){
+    let votreScore = document.querySelector(".zoneScore span")
+    votreScore.innerText = `${score}/${nbMotsTotal}`
+}
 
 
 // 2) quand l'utilisateur tape la réponse et cliquer sur vérifier 
-        
-// function verifierRep(i_correction){
-//     // récupérer les mots tappés
-//     let reponseUtilisateur = document.getElementById("inputReponse").value
-//     // vérifier si c'est ok ? 
-//         if (reponseUtilisateur === reponseCorrect[i_correction]) {
-//             score++
-//             console.log("bravo")
-//         } else {
-//             console.log("raté")
-//         }
-    
-//     console.log(score)
-    
-// }
-
-// verifierRep()
 
 function lancerJeu(){
 
     choixTypeJeu()
+
+    // définition générales 
+  
+    let i_correction = 0   // index mot dans la liste correction 
     
     let zoneProposition = document.querySelector(".zoneProposition")
-
     let BtnVerifierMots = document.getElementById("BtnVerifier")
+
     BtnVerifierMots.addEventListener("click", () => {
 
+
         if (i < listeProposition.length && i_correction < listeCorrect.length){
+
             zoneProposition.innerText = listeProposition[i]
-            
+
             console.log(listeProposition[i],listeCorrect[i_correction])
+            
+            let reponseUtilisateur = document.getElementById("inputReponse")
+            let reponseCorrect = listeCorrect[i_correction]
+
+    // vérifier si c'est ok ? 
+            if (reponseUtilisateur.value === reponseCorrect) {
+                score++
+                console.log("bravo")
+                
+            } else {
+                console.log("raté")
+                  
+            }
+
+            reponseUtilisateur.value = ""
             i++
-            i_correction++
-            // verifierRep(i_correction)
-            // afficherScore(i.nbMotsTotal)
+            i_correction++      
+            afficherScore(score, nbMotsTotal)   
+            affichageMots(listeProposition[i])
+
         } else{
+
             zoneProposition.innerText = "Fin de jeu !"
+            BtnVerifierMots.ariaDisabled = true
         }
+        
+
     })
+
+ 
+
 }
 
 lancerJeu()
